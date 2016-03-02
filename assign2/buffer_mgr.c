d16aba492a2031cc6a63ba8932231a230d765427
mmalviya24 <mmalviya@hawk.iit.edu>
2016-03-02 19:10:32 +0530

    Final comments added

--- assign2/buffer_mgr.c
+++ assign2/buffer_mgr.c
@@ -1,6 +1,4 @@
-//
-// Created by apegadoboureghda on 26/02/16.
-//
+
 
 #include <stdlib.h>
 #include "buffer_mgr.h"
@@ -19,6 +17,33 @@ typedef struct MgmtData {
     int readBuff; // Read from buffer counter
     BM_PageHandle **buffer; // Buffer data
 } MgmtData;
+
+/*
+ * Function Name: initBufferPool
+ *
+ * Description:
+ *	Creates a buffer pool for an existing page file.
+ *      Opens an existing page file.
+ *      Initializes the buffer pool.
+ *      Handles the memory allocation.
+ *
+ * Parameters:
+ *      BM_BufferPool *const bm: Object for buffer pool
+ *	const char *const pageFileName:Name of the page file associated with the buffer pool 
+ *	const int numPages: Number of page frames
+ *	ReplacementStrategy strategy:Page replacement strategy
+ *	void *stratData:Pointer to bookkeeping data
+ *
+ * Return:
+ *      RC: Returned Code
+ *          Should return RC_OK.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/20/2016  Andres Pegado   Initialization
+ *      02/24/2016  Andres Pegado   Updates
+ */
 
 RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName, const int numPages,
                   ReplacementStrategy strategy, void *stratData) {
@@ -64,6 +89,26 @@ RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName, const
     return RC_OK;
 }
 
+/*
+ * Function Name: shutdownBufferPool
+ *
+ * Description:
+ *      Destroys a buffer pool.
+ *      Checks for user using he buffer and throws error RC_WRITE_FAILED.
+ *      Uses forceFlushpool function to move dirty pages to disk.
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: BUffer pool object
+ *
+ * Return:
+ *      RC: Returned Code
+ *          Should return RC_OK.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/20/2016  Mansi Malviya   Initialization
+ */
 RC shutdownBufferPool(BM_BufferPool *const bm) {
 
     MgmtData *data = bm->mgmtData;
@@ -79,6 +124,25 @@ RC shutdownBufferPool(BM_BufferPool *const bm) {
     return RC_OK;
 }
 
+/*
+ * Function Name: forceFlushPool
+ *
+ * Description:
+ *      Writes dirty pages from buffer pool to disk.
+ *      Checks for fixCount to be zero and writes the page onto disk.
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: BUffer pool object
+ *
+ * Return:
+ *      RC: Returned Code
+ *          Should return RC_OK.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/20/2016  Mansi Malviya   Initialization
+ */
 RC forceFlushPool(BM_BufferPool *const bm) {
 
     MgmtData *data=bm->mgmtData;
@@ -103,7 +167,27 @@ RC forceFlushPool(BM_BufferPool *const bm) {
 
     return RC_OK;
 }
-
+/*
+ * Function Name: markDirty
+ *
+ * Description:
+ *      It marks the page dirty.
+ *      Checks for each page on buffer using page handle if not null then marks it dirty.
+ *      Updates the dirtyPin array witht he position of page.
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: Buffer pool object
+ *	BM_PageHandle *const page: Buffer pool page handle 
+ *
+ * Return:
+ *      RC: Returned Code
+ *          Should return RC_OK and no page exist in buffer then returns RC_READ_NON_EXISTING_PAGE.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/21/2016  Andres Pegado   Initialization
+ */
 RC markDirty(BM_BufferPool *const bm, BM_PageHandle *const page) {
     MgmtData *data = bm->mgmtData;
     int y=0;
@@ -123,7 +207,27 @@ RC markDirty(BM_BufferPool *const bm, BM_PageHandle *const page) {
         return RC_READ_NON_EXISTING_PAGE;
     }
 }
-
+/*
+ * Function Name: unpinPage
+ *
+ * Description:
+ *      Unpins a particular page from buffer.
+ *      Takes pageNum as page to be unpinned from buffer.
+ *      Updates the fixCount to decrease the position.
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: Buffer pool object
+ *	BM_PageHandle *const page: Buffer pool page handle 
+ *
+ * Return:
+ *      RC: Returned Code
+ *          Should return RC_OK and no page exist in buffer then returns RC_READ_NON_EXISTING_PAGE.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/21/2016  Andres Pegado   Initialization
+ */
 RC unpinPage(BM_BufferPool *const bm, BM_PageHandle *const page) {
     //Check if it's already on buffer
     int y= 0;
@@ -147,7 +251,27 @@ RC unpinPage(BM_BufferPool *const bm, BM_PageHandle *const page) {
 
 
 }
-
+/*
+ * Function Name: forcePage
+ *
+ * Description:
+ *      Writes the current content of the page back to disk.
+ *      Checks for page existence in buffer.
+ *      Write the data from buffer to disk.
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: Buffer pool object
+ *	BM_PageHandle *const page: Buffer pool page handle 
+ *
+ * Return:
+ *      RC: Returned Code
+ *          Should return RC_OK.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/22/2016  Mansi Malviya   Initialization
+ */
 RC forcePage(BM_BufferPool *const bm, BM_PageHandle *const page) {
 
     SM_FileHandle *fHandle = (SM_FileHandle *) malloc(sizeof(fHandle));
@@ -161,7 +285,29 @@ RC forcePage(BM_BufferPool *const bm, BM_PageHandle *const page) {
     data->writeIO++;
     return RC_OK;
 }
-
+/*
+ * Function Name: pinPage
+ *
+ * Description:
+ *      Pins the page in buffer with the pageNum.
+ *      Checks for page existence in buffer if it existes then mark the position.
+ *      Uses freeFrame for strategy, allocates memory,read the data from pageNum and pin it in buffer.
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: Buffer pool object
+ *	BM_PageHandle *const page: Buffer pool page handle 
+ *	const PageNumber pageNum: Number of the page to be pinned.
+ *
+ * Return:
+ *      RC: Returned Code
+ *          Should return RC_OK.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/22/2016  Andres Pegado   Initialization
+ *      02/26/2016  Bill Molchan    Updates
+ */
 RC pinPage(BM_BufferPool *const bm, BM_PageHandle *const page, const PageNumber pageNum) {
 
 
@@ -206,6 +352,30 @@ RC pinPage(BM_BufferPool *const bm, BM_PageHandle *const page, const PageNumber 
     return RC_OK;
 }
 
+/*
+ * Function Name: freeFrame
+ *
+ * Description:
+ *      Handles replacement startegy to be used for page replacement.
+ *      Uses fifo and lru stat which are arrays defined per each buffer page.
+ *      Checks for dirty page and updates the position .
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: Buffer pool object
+ *	    int pageNum: Number of the page to be replaced.
+ *
+ * Return:
+ *      RC: Returned position of where the page should be replaced.
+ *          
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/22/2016  Andres Pegado   Initialization
+ *      02/24/2016  Mansi Malviya   Updates
+ *      02/26/2016  Bill Molchan    Updates
+ *      02/26/2016  Pratishtha      Updates    
+ */
 
 //TODO
 int freeFrame(int pageNum,BM_BufferPool *const bm){
@@ -250,7 +420,23 @@ int freeFrame(int pageNum,BM_BufferPool *const bm){
     return position;
 
 }
-
+/*
+ * Function Name: getFrameContents
+ *
+ * Description:
+ *      Checks all the pages in the frame and get the content of the page.
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: Buffer pool object
+ *
+ * Return:
+ *      RC: Returned Content of the page.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/28/2016  Bill Molchan   Initialization
+ */
 PageNumber *getFrameContents(BM_BufferPool *const bm) {
 
     PageNumber *contents =(int*) malloc(bm->numPages * sizeof(int));
@@ -264,22 +450,70 @@ PageNumber *getFrameContents(BM_BufferPool *const bm) {
     }
     return contents;
 }
-
+/*
+ * Function Name: getDirtyFlags
+ *
+ * Description:
+ *      Checks all the dirty flags so far occured in the program.
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: Buffer pool object
+ *
+ * Return:
+ *      RC: Returned no. dirty flags.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/1/2016  Bill Molchan       Initialization
+ */
 bool *getDirtyFlags(BM_BufferPool *const bm) {
 
     MgmtData *data=bm->mgmtData;
 
-
     return data->dirtyPin;
 }
 
+/*
+ * Function Name: getFixCounts
+ *
+ * Description:
+ *      Checks all the fix counts so far occured in the program.
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: Buffer pool object
+ *
+ * Return:
+ *      RC: Returned no. of fixcounts.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/1/2016   Pratishtha       Initialization
+ */
 int *getFixCounts(BM_BufferPool *const bm) {
     MgmtData *data=bm->mgmtData;
 
 
     return data->fixCount;
 }
-
+/*
+ * Function Name: getNumReadIO
+ *
+ * Description:
+ *      Checks all the pages read so far in the program.
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: Buffer pool object
+ *
+ * Return:
+ *      RC: Returned no. of pages read.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/1/2016   Pratishtha       Initialization
+ */
 int getNumReadIO(BM_BufferPool *const bm) {
 
         MgmtData *data=bm->mgmtData;
@@ -287,7 +521,23 @@ int getNumReadIO(BM_BufferPool *const bm) {
 
         return data->readIO;
 }
-
+/*
+ * Function Name: getNumReadIO
+ *
+ * Description:
+ *      Checks all the pages written so far in the program.
+ *       
+ * Parameters:
+ *      BM_BufferPool *const bm: Buffer pool object
+ *
+ * Return:
+ *      RC: Returned no. of pages written.
+ *
+ * History:
+ *      Date        Name            Content
+ *      ----------  --------------  ---------------------
+ *      02/1/2016   Mansi Malviya   Initialization
+ */
 int getNumWriteIO(BM_BufferPool *const bm) {
     MgmtData *data=bm->mgmtData;
 
